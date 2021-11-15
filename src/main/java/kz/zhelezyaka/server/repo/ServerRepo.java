effb8b80f4d2721e06a27ef37e1ef6cbc0459b3e
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-11-15 22:11:50 +0600

    Domain Repository

---
+++ src/main/java/kz/zhelezyaka/server/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package kz.zhelezyaka.server.repo;
+
+import kz.zhelezyaka.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
