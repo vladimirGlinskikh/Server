af607ca4235c120fa8f26ca2c785145330e84d3f
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-11-16 06:41:57 +0600

    Application Service

---
+++ src/main/java/kz/zhelezyaka/server/service/ServerService.java
@@ -0,0 +1,19 @@
+package kz.zhelezyaka.server.service;
+
+import kz.zhelezyaka.server.model.Server;
+
+import java.util.Collection;
+
+public interface ServerService {
+    Server create(Server server);
+
+    Server ping(String ipAddress);
+
+    Collection<Server> list(int limit);
+
+    Server get(Long id);
+
+    Server update(Server server);
+
+    Boolean delete(Long id);
+}
---
+++ src/main/java/kz/zhelezyaka/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,39 @@
+package kz.zhelezyaka.server.service.implementation;
+
+import kz.zhelezyaka.server.model.Server;
+import kz.zhelezyaka.server.service.ServerService;
+
+import java.util.Collection;
+
+public class ServerServiceImpl implements ServerService {
+    @Override
+    public Server create(Server server) {
+        return null;
+    }
+
+    @Override
+    public Server ping(String ipAddress) {
+        return null;
+    }
+
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
