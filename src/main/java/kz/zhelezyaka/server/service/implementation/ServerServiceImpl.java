b694c3c6b9ec453a48d80138fec5f013d4e597dc
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-11-17 22:52:23 +0600

    Service Implementation Part 2

--- src/main/java/kz/zhelezyaka/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/kz/zhelezyaka/server/service/implementation/ServerServiceImpl.java
@@ -1,21 +1,22 @@
 package kz.zhelezyaka.server.service.implementation;
 
-import kz.zhelezyaka.server.enumeration.Status;
 import kz.zhelezyaka.server.model.Server;
 import kz.zhelezyaka.server.repo.ServerRepo;
 import kz.zhelezyaka.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
 
+import static java.lang.Boolean.*;
 import static kz.zhelezyaka.server.enumeration.Status.SERVER_DOWN;
 import static kz.zhelezyaka.server.enumeration.Status.SERVER_UP;
+import static org.springframework.data.domain.PageRequest.*;
 
 @RequiredArgsConstructor
 @Service
@@ -44,22 +45,27 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting sever by ID: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
 
     private String setServerImageUrl() {
