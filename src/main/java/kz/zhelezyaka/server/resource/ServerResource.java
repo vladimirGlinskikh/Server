6b26ca73c8404a64fb23b112abb8326e35a4d8bc
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-11-18 03:06:05 +0600

    HTTP Resource Part 1

---
+++ src/main/java/kz/zhelezyaka/server/resource/ServerResource.java
@@ -0,0 +1,33 @@
+package kz.zhelezyaka.server.resource;
+
+import kz.zhelezyaka.server.model.Response;
+import kz.zhelezyaka.server.service.implementation.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static java.time.LocalDateTime.*;
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build());
+    }
+}
